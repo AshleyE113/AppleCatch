8dc465b7cbf0de2635b868f0195995d63c9e63f9
alexandra <alexandraabrego9@gmail.com>
2021-05-08 23:36:11 -0400

    Updated Intersect Method

--- Player.java
+++ Player.java
@@ -2,16 +2,18 @@ import processing.core.*;
 
 class Player extends PApplet {
 	//constants
-	float height;
-	float width;
-	float posX;
-	float posY;
-	
+//	float height;
+//	float width;
+//	float posX;
+//	float posY;
+	int lives;
+
 	Player(){
-		height = 75;
-		width = 100;
-		posX = 250;
-		posY = 0;
+//		height = 75;
+//		width = 100;
+//		posX = 250;
+//		posY = 0;
+		lives = 3;
 	}
 	/*
 	public void setup() {
@@ -19,7 +21,7 @@ class Player extends PApplet {
 		for(int i = 0; i < player_img; i++) {
 			playerImages[i] = loadImage("spr_player"+i+".png");
 		}
-		
+
 	}
 	*/
 	public void drawPlayer() {
@@ -33,14 +35,14 @@ class Player extends PApplet {
 			*/
 		//currentFrame++;
 		//System.out.print(currentFrame);
-		
+
 		//if (currentFrame == loopFrames-1) {
 		//	currentFrame = 0;
 		//}
 	}
-	
+
 	public void Movement() {
-		// if player presses left key, move by 50px, presses right key move by 50px, starts right at middle 
+		// if player presses left key, move by 50px, presses right key move by 50px, starts right at middle
 		//currentFrame = (currentFrame + 0.5)%3;
 		/*
 		if (keyPressed) {
@@ -49,34 +51,30 @@ class Player extends PApplet {
 					inMotion = true;
 					System.out.print("Left");
 					posX--;
-				} 
+				}
 				else if (keyCode == RIGHT) {
 					inMotion = true;
 					posX++;
-					System.out.print("Right");					
+					System.out.print("Right");
 				}
 			}
 		}
 		*/
 	}
 
-	public boolean Intersect(FallingObject fallingObject) {
-		// Finds The Distance Between The Player And Apple
-		float distance = dist(posX, posY, fallingObject.xPosition, fallingObject.yPosition);
-
-		if (distance < height/2 + fallingObject.Radius) {
-			// Player And Apple Intersected
+	public boolean Intersect(FallingObject fallingObject, float xLPosition, float xRPosition, float yPos) {
+		if((fallingObject.yPosition == yPos) && ( fallingObject.xPosition >= xLPosition && fallingObject.xPosition <= xRPosition) ){
 			return true;
 		}
-		else {
-			// Player And Apple Did Not intersect
+		else{
 			return false;
 		}
 	}
-	
+
 	//public static void main(String[] args) {
 	//	PApplet.main("Player");
 	//}
 }
 
-	
+
+
