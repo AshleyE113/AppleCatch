7e70ca95b91bc20f967b976d473fea7ceefacc0e
AshleyE113 <ase330@nyu.edu>
2021-05-09 01:21:32 -0400

    //Integrated all the changes! Currently trying to fix an issue so don't worry about some of the vals at the end

--- src/Game.java
+++ src/Game.java
@@ -4,20 +4,21 @@ public class Game extends PApplet {
 
     public static void main(String[] args) {
         // The argument passed to main must match the class name
-    	
+
         PApplet.main("Game");
     }
-
+    
     Pause pauseButton;
-    Timer timer;
-    Timer waitTime;
+    Timer timer; // For Game Level
+    Timer waitTime; // For Apples
+    Timer gameStatus; // For status text
     Player player;
     PImage playerImages[];
-	//public static PImage player;
+    //public static PImage player;
     FallingObject[] apples = new FallingObject[100];
     int activeApples = 0; // Apples That Have Been Created
+    int Level = 1; // To Keep Track Of What Level The Player Is On
     public static int minApples = 0; // Minimum Apples Required
-    int totalObjects = 0;
     public static PImage Apple;
     public static PImage bg_game;
     public static PImage TitleScreen;
@@ -29,11 +30,12 @@ public class Game extends PApplet {
     float width = 225;
     float posX = 250;
     float posY = 225;
+    float yBasket = 320;
+    float xLeftBasket = 315;
+    float xRightBasket = 423;
     int currentFrame = 0;
-	int player_img = 3;
-	int toWin = 20;
-	int playerLives = 3;
-	boolean inMotion = false;
+    int player_img = 3;
+    boolean inMotion = false;
 	boolean paused = false;
 	boolean playGame = false;
 	boolean mouseInStart = false;
@@ -44,7 +46,6 @@ public class Game extends PApplet {
         size(500, 500);
     }
 
-
     public void setup() {
     	frameRate(60);
         Apple = loadImage("apple.png");
@@ -54,159 +55,208 @@ public class Game extends PApplet {
         WinScreen = loadImage("AC_Win.png");
         StartB = loadImage("StartB.png");
 		RestartB = loadImage("RestartB.png");
-
-        for (int i = 0 ; i< apples.length;i++){
-            apples[i] = new FallingObject(this);
-        }
-
-        waitTime = new Timer(1000);// 1 Second
+		
+		//Timers
+        gameStatus = new Timer(3000);
+        waitTime = new Timer(3000);// 3 Seconds
         waitTime.start();
         timer = new Timer(40000); // 40 Seconds
         timer.start();
         smooth();
+        
+        //Initialiing Player info
         player = new Player();
-    	playerImages = new PImage[player_img];
-		for(int i = 0; i < player_img; i++) {
-			playerImages[i] = loadImage("spr_player"+i+".png");
-		}
+        playerImages = new PImage[player_img];
+        for(int i = 0; i < player_img; i++) {
+            playerImages[i] = loadImage("spr_player"+i+".png");
+        }
     }
-    
+
     public void keyPressed() {
- 		if (key == CODED) {
- 			if (keyCode == LEFT) {
- 				inMotion = true;
-				//System.out.println("Left");
-				posX = posX - 5;
-			} 
- 			else if (keyCode == RIGHT) {
- 				inMotion = true;
- 				posX = posX + 5;
- 				//System.out.println("Right");					
- 			}
- 			else {
- 				inMotion = false;
- 			}
- 			
- //			/*
- 			if (key == CODED) {
- 				if (keyCode == SHIFT){
+
+        if (key == CODED) {
+        	
+            if (keyCode == LEFT) {
+                inMotion = true;
+                //System.out.println("Left");
+                posX = posX - 5;
+                xLeftBasket -= 5;
+                xRightBasket -= 5;
+            }
+            else if (keyCode == RIGHT) {
+                inMotion = true;
+                posX = posX + 5;
+                xLeftBasket += 5;
+                xRightBasket += 5;
+                //System.out.println("Right");
+            }else {
+                inMotion = false;
+            }
+           
+           if (key == CODED) {
+            if (keyCode == SHIFT){
 				paused = !paused;
 				if (paused) {
 					noLoop();
 				} else {
 					loop();
 				}
- 			}
-				//*/
-		}  
+            }
+       }
     }
-}
+  }
     
-public void StartScreen() {
-	background(TitleScreen);
-	image(StartB, 10, 420);
-	System.out.println("Start!");
-	
-	if ((mouseX >= 4 && mouseY >= 418) && (mouseX <= 138 && mouseY <= 482)){
-		mouseInStart = true;
-	}
-	
-}
+    public void StartScreen() {
+    	background(TitleScreen);
+    	image(StartB, 10, 420);
+    	
+    	//System.out.println("Start!");
+    	
+    	if ((mouseX >= 10 && mouseY >= 419) && (mouseX <= 138 && mouseY <= 482)){
+    		text("TEST", 10, 419);
+    		text("TEST2", 138, 482);
+    		mouseInStart = true;
+    	}
+    	
+    }
 
-public void LoseScreen() {
-	background(LossScreen);
-	image(RestartB, 180, 355);
-	
-	if ((mouseX >= 176 && mouseY >= 352) && (mouseX <= 305 && mouseY <= 416)){
-		mouseInRestart = true;
-	}
-	
-}
+    public void LoseScreen() {
+    	background(LossScreen);
+    	image(RestartB, 180, 355);
+    	
+    	if ((mouseX >= 176 && mouseY >= 352) && (mouseX <= 305 && mouseY <= 416)){
+    		mouseInRestart = true;
+    	}
+    	
+    }
 
-public void WinScreen() {
-	background(WinScreen);
-	image(RestartB, 180, 400);
-	
-	if ((mouseX >= 179 && mouseY >= 398) && (mouseX <= 305 && mouseY <= 460)){
-		mouseInRestart = true;
-	}
-	
-}
+    public void WinScreen() {
+    	background(WinScreen);
+    	image(RestartB, 180, 400);
+    	
+    	if ((mouseX >= 179 && mouseY >= 398) && (mouseX <= 305 && mouseY <= 460)){
+    		mouseInRestart = true;
+    	}
+    	
+    }
 
-
-
-public void mouseClicked() {
-	if (mouseInStart) {
-		playGame = true;
-	}
-	
-	if (mouseInRestart) {
-		ToStart = true;
-	}
-}
-    
+    public void mouseClicked() {
+    	if (mouseInStart) {
+    		playGame = true;
+    		System.out.println("Clicked!");
+    	}
+    	
+    	if (mouseInRestart) {
+    		ToStart = true;
+    	}
+    }
 
     public void draw() {
     	StartScreen();
-    if (playGame) {
-        background(bg_game);
-        fill(50, 10, 10, 150);
-        text("Level: ", 10, 20);
-        text("Score: " , 10, 30);
-        text(FallingObject.objectsCaught, 50, 30);
-        text("Apples Required: 20", 10, 40);
-        text((timer.passedTime/1000), 10, 50);
-        text("Lives: ", 10, 60);
-        
-		if (inMotion) {
-		 currentFrame = (int) Math.abs((posX)%3); //Makes ani happen
-		 image(playerImages[currentFrame], posX, posY, width, height);		
-		}  else {
-			image(playerImages[1], posX, posY, width, height);
-		}
-		
-		if (posX > 510) {
-			posX = -10;
-		} else if (posX < -15) {
-			posX = 505;
-		}
-       
+        if (playGame) {
+	        background(bg_game);
+	        
+	        if (inMotion) {
+	            currentFrame = (int) Math.abs((posX)%3); //Makes ani happen
+	            image(playerImages[currentFrame], posX, posY, width, height);
+	
+	        } else {
+	        	image(playerImages[1], posX, posY, width, height);
+	        }
+	        
+	        /*
+			if (posX > 505) {
+				posX = 0;
+			} else if (posX < 0) {
+				posX = 500;
+			}*/
+	
+	        switch (Level) {
+	            case 1:
+	                minApples = 12;
+	                timer = new Timer(40000); // 40 Seconds
+	                FallingObject.Velocity = 2;
+	                break;
+	            case 2:
+	                minApples = 13;
+	                timer = new Timer(35000); // 35 Seconds
+	                FallingObject.Velocity = 1.2;
+	                break;
+	            case 3:
+	                minApples = 15;
+	                timer = new Timer(30000); // 30 Seconds
+	                FallingObject.Velocity = 1.4;
+	                break;
+	            case 4:
+	                minApples = 16;
+	                timer = new Timer(25000); // 25 Seconds
+	                FallingObject.Velocity = 1.6;
+	                break;
+	            case 5:
+	                minApples = 18;
+	                timer = new Timer(20000); // 20 Seconds
+	                FallingObject.Velocity = 2;
+	                break;
+	        }
+	
+	        fill(0, 0, 0);
+	        textSize(12);
+	        text("Level: " +  + Level, 20, 20);
+	        text("Score: " + FallingObject.objectsCaught, 20, 35);
+	        text("Apples Required: " + minApples, 20, 50);
+	        text((timer.passedTime/1000), 20, 56);
+	        text("Lives: " + player.lives, 20, 80);
+	
+	        if (waitTime.isFinished()){
+	            // Populate
+	            apples[activeApples] = new FallingObject(this);
+	            // Add to the Apples Ready
+	            activeApples++;
+	            // Start Over
+	            if (activeApples >= apples.length) {
+	                activeApples= 0;
+	            }
+	            // Wait Time For Next Apple To Be Created
+	            waitTime.start();
+	        }
+	
+	        // Only Apples That Have Been created Can Fall
+	        // Will Make On Apple Fall At A Time Since The Number Of Active Apples Is Always Changing by 1
+	        for (int i = 0 ; i< activeApples;i++) { //int i = 0 ; i< apples.length;i++
+	            apples[i].display();
+	            apples[i].moveDown();
+	            if (player.Intersect(apples[i], xLeftBasket, xRightBasket, yBasket)){
+	                apples[i].caught();
+	                FallingObject.objectsCaught++;
+	            }
+	            else if (apples[i].reachedBottom()){
+	                player.lives--;
+	            }
+	
+	        }
+	        
+	        //TEMPORARY! DON'T WORRY ABOUT IT! TRYING TO FIX AN ISSUE
+	        if (FallingObject.objectsCaught >= 5 && (!timer.isFinished())){
+	        	WinScreen();
+	        	
+	        	if (ToStart) {
+	        		StartScreen();
+	        		mouseInStart = false;
+	            	playGame = false;
+	        		//System.out.println("Back to Start!");
+	        	}
+	        } else if (FallingObject.objectsCaught <= 5 && timer.isFinished()) {
+	        	LoseScreen();
+	        	if (ToStart) {
+	        		StartScreen();
+	        		mouseInStart = false;
+	            	playGame = false;
+	        		//System.out.println("Back to Start!");
+	        	}
+	        }
+	
+	    }
+  }
 
-        if (waitTime.isFinished()) {
-            // Populate
-            apples[activeApples] = new FallingObject(this);
-            // Add to the Apples Ready
-            activeApples++;
-            // Start Over
-            if (activeApples >= apples.length) {
-                activeApples = 0;
-            }
-            // Wait Time For Next Apple To Be Created
-            waitTime.start();
-        }
-
-        for (int i = 0; i < activeApples; i++) { //int i = 0 ; i< apples.length;i++
-            apples[i].display();
-            apples[i].moveDown();
-            if (player.Intersect(apples[i])) {
-                apples[i].caught();
-                FallingObject.objectsCaught++;
-                //fill(50, 10, 10, 150);
-                System.out.println(FallingObject.objectsCaught);
-            }
-        }
-        
-        if (FallingObject.objectsCaught >= 20 && (!timer.isFinished())){
-        	WinScreen();
-        	if (ToStart) {
-        		StartScreen();
-        	}
-        } else if (FallingObject.objectsCaught <= 20 && timer.isFinished()) {
-        	LoseScreen();
-        	if (ToStart) {
-        		StartScreen();
-        	}
-        }
-    }
-   }
 }
+
